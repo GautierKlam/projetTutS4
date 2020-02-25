4613672465884fa984ef564558e8c53f81a9b0bb
GautierKlam <55312620+GautierKlam@users.noreply.github.com>
2020-02-25 11:33:15 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+export default {
+  mysql: {
+  host:'devbdd.iutmetz.univ-lorraine.fr',
+  port:3306,
+  user:'sizaire1u_appli',
+  data:'sizaire1u_projets4',
+  password:'31806635'
+  
+  }
+  
+  }
